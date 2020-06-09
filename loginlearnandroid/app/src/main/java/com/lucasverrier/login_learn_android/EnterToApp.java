cc4c767645130ce2c81013b2b53b994d105df348
byebyebabayaga <lucasverrier1@gmail.com>
2020-06-09 18:07:18 +0200

    pepein avec l'adaptateur
    j'ai un pepein avec l'adapteur

--- loginlearnandroid/app/src/main/java/com/lucasverrier/login_learn_android/EnterToApp.java
+++ loginlearnandroid/app/src/main/java/com/lucasverrier/login_learn_android/EnterToApp.java
@@ -1,14 +1,35 @@
 package com.lucasverrier.login_learn_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class EnterToApp extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_enter_to_app);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
+
+        ArrayList<Cartepokemon> exampleItems = new ArrayList<>();
+        
     }
 }
