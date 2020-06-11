da59fdb593ce6114d1fb4ec4db3141c586fe1c24
byebyebabayaga <lucasverrier1@gmail.com>
2020-06-11 19:19:09 +0200

    last one
    last one

--- loginlearnandroid/app/src/main/java/com/lucasverrier/login_learn_android/MyAdapter.java
+++ loginlearnandroid/app/src/main/java/com/lucasverrier/login_learn_android/MyAdapter.java
@@ -18,6 +18,7 @@ public  class MyAdapter extends
         exampleList.add(new Cartepokemon(R.drawable.ic_android_black, "Legumos", "epine"));
         exampleList.add(new Cartepokemon(R.drawable.ic_android_black, "ChatChien", "griffe"));
 
+        return exampleList;
     }
 
     @NonNull
@@ -36,22 +37,18 @@ public  class MyAdapter extends
         return 0;
     }
 
-    // Provide a direct reference to each of the views within a data item
-    // Used to cache the views within the item layout for fast access
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        // Your holder should contain a member variable
-        // for any view that will be set as you render a row
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
         public TextView nameTextView;
 
 
         private TextView description;
         private TextView attacks;
 
-        // We also create a constructor that accepts the entire item row
-        // and does the view lookups to find each subview
+
         public ViewHolder(View itemView) {
-            // Stores the itemView in a public final member variable that can be used
-            // to access the context from any ViewHolder instance.
+
             super(itemView);
 
             description = (TextView) itemView.findViewById(R.id.mladescription);
